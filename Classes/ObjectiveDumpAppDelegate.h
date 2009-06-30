613e9977849fb6513532b7c6c2a6f1025ac8af56
Anthony Mittaz <sync@me.com>
2009-06-30 22:37:35 +1000

    added default project

---
+++ Classes/ObjectiveDumpAppDelegate.h
@@ -0,0 +1,31 @@
+//
+//  ObjectiveDumpAppDelegate.h
+//  ObjectiveDump
+//
+//  Created by Anthony Mittaz on 30/06/09.
+//  Copyright Anthony Mittaz 2009. All rights reserved.
+//
+
+@interface ObjectiveDumpAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+- (IBAction)saveAction:sender;
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, readonly) NSString *applicationDocumentsDirectory;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
+
